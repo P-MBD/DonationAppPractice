4da67345b1eecd11d00b45d518535aea0eb4ba71
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-12-25 12:01:07 +0330

    125 - Persisting Store-main

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {Pressable, SafeAreaView, Text} from 'react-native';
+
 // Importing the useSelector and useDispatch hooks from the React Redux library
 // The useSelector hook allows us to select and retrieve data from the store
 // The useDispatch hook allows us to dispatch actions to update the store
 import {useDispatch, useSelector} from 'react-redux';
+
 import {updateFirstName} from '../../redux/reducers/User';
+
 import Header from '../../components/Header/Header';
+
 import globalStyle from '../../assets/styles/globalStyle';
+
 const Home = () => {
   // Using the useSelector hook to select the "user" slice of the store
   // This will return the user object containing firstName, lastName and userId fields
@@ -15,7 +20,9 @@ const Home = () => {
   // Using the useDispatch hook to get a reference to the dispatch function
   // This function allows us to dispatch actions to update the store
   const dispatch = useDispatch();
+
   console.log(user);
+
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <Header title={user.firstName + ' ' + user.lastName} />
