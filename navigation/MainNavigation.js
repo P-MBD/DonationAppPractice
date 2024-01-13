c9ca8ead5071fb7785d9595516f1977e4eddd8a0
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-01-13 09:23:46 +0330

    144 - Registration Screen

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -3,15 +3,17 @@ import {Routes} from './Routes';
 import Home from '../screens/Home/Home';
 import SingleDonationItem from '../screens/SingleDonationItem/SingleDonationItem';
 import Login from '../screens/Login/Login';
+import Registration from '../screens/Registration/Registration';
 const Stack = createStackNavigator();
 
 const MainNavigation = () => {
   return (
     <Stack.Navigator 
-    initialRouteName={Routes.login}
+    initialRouteName={Routes.Login}
     screenOptions={{header: () => null, headerShown: false}}
     >
       <Stack.Screen name={Routes.Login} component={Login} />
+      <Stack.Screen name={Routes.Registration} component={Registration} />
       <Stack.Screen name={Routes.Home} component={Home} />
       <Stack.Screen name={Routes.SingleDonationItem} component={SingleDonationItem} />
     </Stack.Navigator>
