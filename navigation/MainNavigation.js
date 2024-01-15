106fdcc6ab189df413841633c196de97b5e5d0a0
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-01-15 11:56:46 +0330

    154 - Navigation Flows Authenticated Non Authenticated

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -4,20 +4,30 @@ import Home from '../screens/Home/Home';
 import SingleDonationItem from '../screens/SingleDonationItem/SingleDonationItem';
 import Login from '../screens/Login/Login';
 import Registration from '../screens/Registration/Registration';
+
 const Stack = createStackNavigator();
 
-const MainNavigation = () => {
+export const NonAuthenticated = () => {
   return (
-    <Stack.Navigator 
-    initialRouteName={Routes.Login}
-    screenOptions={{header: () => null, headerShown: false}}
-    >
+    <Stack.Navigator
+      initialRouteName={Routes.Login}
+      screenOptions={{header: () => null, headerShown: false}}>
       <Stack.Screen name={Routes.Login} component={Login} />
       <Stack.Screen name={Routes.Registration} component={Registration} />
-      <Stack.Screen name={Routes.Home} component={Home} />
-      <Stack.Screen name={Routes.SingleDonationItem} component={SingleDonationItem} />
     </Stack.Navigator>
   );
 };
 
-export default MainNavigation;
+export const Authenticated = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName={Routes.Home}
+      screenOptions={{header: () => null, headerShown: false}}>
+      <Stack.Screen name={Routes.Home} component={Home} />
+      <Stack.Screen
+        name={Routes.SingleDonationItem}
+        component={SingleDonationItem}
+      />
+    </Stack.Navigator>
+  );
+};
