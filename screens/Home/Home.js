88511974a57918be4667a871402ef803574c0269
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-01-15 12:58:43 +0330

    155 - Logging Out English

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -80,11 +80,20 @@ const Home = ({navigation}) => {
               />
             </View>
           </View>
-          <Image
-            source={{uri: user.profileImage}}
-            style={style.profileImage}
-            resizeMode={'contain'}
-          />
+          <View>
+            <Image
+              source={{uri: user.profileImage}}
+              style={style.profileImage}
+              resizeMode={'contain'}
+            />
+            <Pressable
+              onPress={async () => {
+                dispatch(resetToInitialState());
+                await logOut();
+              }}>
+              <Header type={3} title={'Logout'} color={'#156CF7'} />
+            </Pressable>
+          </View>
         </View>
         <View style={style.searchBox}>
           <Search />
